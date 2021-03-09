6832e432a081efbd522ceb0805d71b6f6056695f
Gabriele Garlaschelli <gabriele.garlaschelli.photo@gmail.com>
2021-03-09 14:45:16 +0100

    change url

--- xmlConverter/converter/urls.py
+++ xmlConverter/converter/urls.py
@@ -6,9 +6,9 @@ from django.views.generic import RedirectView
 from . import views
 
 urlpatterns = [
-    path('upload/', views.upload, name="upload"),
+    path('upload/eur1/', views.upload, name="upload"),
     path('message/', views.message, name="message"),
     path('convert_in_word/', views.convert_in_word, name="convert_in_word"),
     path('convert_in_pdf/', views.convert_in_pdf, name="convert_in_pdf"),
-    re_path(r'^$', RedirectView.as_view(url='upload/', permanent=False), name='index'),
+    re_path(r'^$', RedirectView.as_view(url='upload/eur1/', permanent=False), name='index'),
 ]
